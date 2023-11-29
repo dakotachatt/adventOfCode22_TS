9d5c38b78d95bc19961c9511a1aabe2a2fa4df0e
dakotachatt <dvchatt@gmail.com>
2023-11-29 07:46:42 -0700

    Dec6 complete

--- src/day-6/a.spec.ts
+++ src/day-6/a.spec.ts
@@ -2,6 +2,6 @@ import { day6a } from './a';
 
 describe('Puzzle 6 a', () => {
   test('given example', async () => {
-    expect(await day6a('data/day-6/a-test.txt')).toBe(0);
+    expect(await day6a('data/day-6/a-test.txt')).toBe(6);
   });
 });
--- src/day-6/a.ts
+++ src/day-6/a.ts
@@ -3,6 +3,18 @@ import chalk from 'chalk';
 
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
+  const signalStream = data[0];
+
+  for(let i = 14; i < signalStream.length; i++) {
+    const substringToCompare = signalStream.slice(i - 14, i);
+
+    const sortedSubstringArray = substringToCompare.split('').sort().join("").match(/(.)\1+/g);
+
+    if(!sortedSubstringArray) {
+      return i;
+    }
+  }
+
   return 0;
 }
 
